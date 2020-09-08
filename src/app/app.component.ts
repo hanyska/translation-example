dc07e2d0f27d9fd2e48d17b6e000da1543b5a696
anna.janusz <anna.janusz@loycon.pl>
2020-09-08 15:05:50 +0200

    Added languages service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import {LanguageService} from './language.service';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,28 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'pl']);
-    translate.setDefaultLang('en');
+  availableLanguage;
+
+  constructor(public translate: TranslateService,
+              private langService: LanguageService) {
+    this.getLingohubLangs();
+  }
+
+  private getPoEditorLangs() {
+    this.langService.getPoEditorLangs().subscribe(data => {
+      console.log(data.languages);
+      this.availableLanguage = data.languages.map(lang => lang.code);
+    }, err => {
+      console.log(err);
+    });
+  }
+
+  private getLingohubLangs() {
+    this.langService.getLingohubLangs().subscribe((data: any) => {
+      console.log(data.project_locales);
+      this.availableLanguage = data.project_locales;
+    }, err => {
+      console.log(err);
+    });
   }
 }
--- src/app/language.service.ts
+++ src/app/language.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {appConfig} from '../assets/appConfig';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
+  private headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+  poeditorUrl = 'poeditor/languages/list';
+  lingohubUrl = `lingohub/${appConfig.lingoHubUsername}/projects/${appConfig.lingoHubProjectName}?auth_token=${appConfig.lingoHubToken}`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPoEditorLangs() {
+    const body = new HttpParams()
+      .set('api_token', appConfig.poEditorToken)
+      .set('id', appConfig.poEditorProjectId);
+
+    return this.http
+      .post(this.poeditorUrl, body.toString(), {headers: this.headers})
+      .pipe(
+        map((data: any) => data.result)
+      );
+  }
+
+
+  getLingohubLangs() {
+    return this.http.get(this.lingohubUrl);
+  }
+
+
+
+
 }
